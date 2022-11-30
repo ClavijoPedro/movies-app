d6e97770f82def3032eac2d72138ba4a0233418e
Pedro Clavijo <clavijopedro.dev@gmail.com>
2022-11-29 21:58:05 -0300

    MoviesApp-add gh-pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://clavijopedro.github.io/movies-app/",
-  plugins: [react()]
-})
+	base: '/movies-app/',
+	plugins: [react()],
+});
